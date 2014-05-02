7c11cccdfd2229363ef15f08de67ddc2bbb68540
n.b.q.x <nbqxnbq@gmail.com>
2014-05-02 16:05:12 +0900

    fix for multiple files

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var fs = require('fs'),
 
 module.exports = function(opts){
   opts = _.defaults(opts || {}, {timeout:5000});
-  var xslt = saxon(opts.jarPath,opts.xslPath,{timeout:opts.timeout});
 
   function transform(file,e,next){
     var self = this;
+    var xslt = saxon(opts.jarPath,opts.xslPath,{timeout:opts.timeout});
     var path = file.path;
     fs.createReadStream(path).pipe(xslt)
       .on('data',function(cont){
